d3d57c21154686b4aacfe658423e6108a3a837a4
Ahmed Emad <ahmed39652003@gmail.com>
2019-12-06 21:57:55 +0200

    init

--- Koshkie/settings.py
+++ Koshkie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kc@z(#f$f&61_ghsnt779%3_q-#-gj*%4dkqtrl=mv!24pse=#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drivers',
+    'accounts',
+    'orders',
+    'shops',
+    'products',
 ]
 
 MIDDLEWARE = [
