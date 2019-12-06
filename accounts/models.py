7fa52a8fa38504f29bdca9c818563103c6cb0f8e
Ahmed Emad <ahmed39652003@gmail.com>
2019-12-06 22:29:26 +0200

    add user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import string
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+import random
+
+
+def profile_photo_upload(instance, filename):
+    res = ''.join(random.choices(string.ascii_letters, k=30))
+    return 'accounts/{0}'.format(res)
+
+
+class UserProfileModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Profile")
+    profile_photo = models.ImageField(upload_to=profile_photo_upload)
