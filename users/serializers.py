f7cb86232e61f4679b01815c61fa9937ed35678f
Ahmed Emad <ahmed39652003@gmail.com>
2019-12-20 19:16:56 +0200

    Finished Serializers für users app

--- users/serializers.py
+++ users/serializers.py
@@ -26,12 +26,24 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
-        user = User.objects.create(**user_data)
+        user = User.objects.create(**user_data, username=user_data['email'])
         user_profile = UserProfileModel.objects.create(user=user, **validated_data)
         return user_profile
 
     def update(self, instance, validated_data):
-        pass
+        instance.profile_photo = validated_data.get('profile_photo', instance.profile_photo)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.save()
+
+        user_data = validated_data.pop('user')
+        user = instance.user
+        user.first_name = user_data.get('first_name', user.first_name)
+        user.last_name = user_data.get('last_name', user.last_name)
+        user.email = user_data.get('email', user.email)
+        user.set_password(validated_data.get('password', user.password))
+        user.save()
+
+        return instance
 
 
 class UserAddressSerializer(serializers.ModelSerializer):
@@ -41,9 +53,3 @@ class UserAddressSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'special_notes': {'required': False}
         }
-
-    def create(self, validated_data):
-        pass
-
-    def update(self, instance, validated_data):
-        pass
