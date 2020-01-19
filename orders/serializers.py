67d8204bfabd82ba7e54f494d92311a4c6e7b44f
Ahmed Emad <ahmed39652003@gmail.com>
2020-01-19 16:44:45 +0200

    added validation for order driver availability

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,4 +1,4 @@
-#  Copyright (c) Code Written and Tested by Ahmed Emad in 18/01/2020, 21:51
+#  Copyright (c) Code Written and Tested by Ahmed Emad in 19/01/2020, 16:44
 from abc import ABC
 from math import acos, cos, sin, radians
 
@@ -49,7 +49,7 @@ class OrderAddressSerializer(serializers.ModelSerializer):
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    ordered_product = ProductSerializer(read_only=True, keep_only=('id', 'title'))
+    ordered_product = ProductSerializer(read_only=True, keep_only=('id', 'title'), source='product')
     product = serializers.PrimaryKeyRelatedField(write_only=True,
                                                  queryset=ProductModel.objects.all())
 
@@ -135,6 +135,26 @@ class OrderDetailSerializer(serializers.ModelSerializer):
                             'final_price', 'delivery_fee', 'vat')
 
     def validate(self, attrs):
+        user_longitude = attrs['shipping_address']['location_longitude']
+        user_latitude = attrs['shipping_address']['location_latitude']
+
+        min_active_time = timezone.now() - timezone.timedelta(seconds=10)
+        driver_available = DriverProfileModel.objects.annotate(distance=
+                                                               6367 * Acos(Cos(Rad(float(user_latitude))) *
+                                                                           Cos(Rad(F('live_location_longitude'))) *
+                                                                           Cos(Rad(F('live_location_latitude')) -
+                                                                               Rad(float(user_longitude))
+                                                                               ) +
+                                                                           Sin(Rad(float(user_latitude))) *
+                                                                           Sin(Rad(F('live_location_latitude')))
+                                                                           )
+                                                               ).filter(distance__lte=2.5, is_busy=False,
+                                                                        last_time_online__gte=min_active_time
+                                                                        ).exists()
+
+        if not driver_available:
+            raise serializers.ValidationError("there are no drivers in your area")
+
         for item in attrs['items']:
             product = item['product']
             shop = product.shop
@@ -143,8 +163,6 @@ class OrderDetailSerializer(serializers.ModelSerializer):
                 shops.append(shop)
                 shop_longitude = shop.address.location_longitude
                 shop_latitude = shop.address.location_latitude
-                user_longitude = attrs['shipping_address']['location_longitude']
-                user_latitude = attrs['shipping_address']['location_latitude']
                 distance = 6367 * acos(cos(radians(float(user_latitude))) *
                                        cos(radians(shop_longitude)) *
                                        cos(radians(shop_latitude) -
@@ -173,7 +191,8 @@ class OrderDetailSerializer(serializers.ModelSerializer):
             choices = item.pop('choices', [])
             add_ons_ids = item.pop('add_ons_sorts', [])
             order_item = OrderItemModel.objects.create(**item)
-            order_item.product.num_sold.update(num_sold=F('num_sold') + 1)
+            order_item.product.num_sold = F('num_sold') + 1
+            order_item.product.save()
             for choice in choices:
                 option_group = order_item.product.option_groups.get(sort=choice['option_group_id'])
                 choosed_option = option_group.options.get(sort=choice['choosed_option_id'])
@@ -220,7 +239,9 @@ class OrderDetailSerializer(serializers.ModelSerializer):
                                                      ).filter(distance__lte=2.5, is_busy=False,
                                                               last_time_online__gte=min_active_time
                                                               ).order_by('distance')[0]
-        driver.update(is_busy=True)
+        driver.is_busy = True
+        driver.save()
+
         order = OrderModel.objects.create(driver=driver, shipping_address=shipping_address,
                                           final_price=final_price, delivery_fee=delivery_fee,
                                           vat=vat, subtotal=subtotal, **validated_data)
