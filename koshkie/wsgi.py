e09484bad7fecbc1aaaec992d1590a2dc64e0705
Ahmed Emad <ahmed39652003@gmail.com>
2020-01-29 23:25:01 +0200

    small naming bug fixed

--- koshkie/wsgi.py
+++ koshkie/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Koshkie.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koshkie.settings')
 
 application = get_wsgi_application()
