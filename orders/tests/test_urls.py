c44c44aa9e1acb11113821ee345b325a4a4a841b
Ahmed Emad <ahmed39652003@gmail.com>
2020-02-11 22:16:04 +0200

    added urls and urls tests for orders app

--- orders/tests/test_urls.py
+++ orders/tests/test_urls.py
@@ -1,1 +1,21 @@
-#  Copyright (c) Code Written and Tested by Ahmed Emad in 11/02/2020, 20:48
+#  Copyright (c) Code Written and Tested by Ahmed Emad in 11/02/2020, 22:16
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from orders.views import OrderView
+
+
+class OrdersTest(TestCase):
+    """UnitTest for orders urls"""
+
+    def test_orders_list(self):
+        """test for orders list url"""
+        url = reverse('orders:orders-list')
+        self.assertEqual(resolve(url).func.__name__,
+                         OrderView.as_view({'get': 'list'}).__name__)
+
+    def test_orders_detail(self):
+        """test for orders detail url"""
+        url = reverse('orders:orders-detail', kwargs={'pk': '1'})
+        self.assertEqual(resolve(url).func.__name__,
+                         OrderView.as_view({'get': 'retrieve'}).__name__)
--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,11 @@
-#  Copyright (c) Code Written and Tested by Ahmed Emad in 10/01/2020, 18:25
+#  Copyright (c) Code Written and Tested by Ahmed Emad in 11/02/2020, 22:16
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from orders.views import OrderView
 
 orders_router = DefaultRouter()
-orders_router.register('', OrderView, 'orders')
+orders_router.register('', OrderView, basename='orders')
 
 app_name = 'orders'
 
